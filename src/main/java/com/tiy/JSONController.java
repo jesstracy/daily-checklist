0eaa12a8a403e3b5157cc8c9af78c4ef60f6069a
Jessica Tracy <j.tracy916@gmail.com>
2016-11-15 15:56:01 -0500

    login end-to-end

--- src/main/java/com/tiy/JSONController.java
+++ src/main/java/com/tiy/JSONController.java
@@ -17,6 +17,7 @@ public class JSONController {
 
     @RequestMapping(path = "/register.json", method = RequestMethod.POST)
     public LoginRegReturnContainer register(@RequestBody User newUser) {
+        System.out.println("\nIn register method in JSON controller");
         LoginRegReturnContainer loginRegReturnContainer = null;
         try {
             conn = DriverManager.getConnection("jdbc:h2:./main");
@@ -27,7 +28,6 @@ public class JSONController {
             ResultSet resultSet = stmt.executeQuery();
             if (!resultSet.next()) {
                 //No data in result set, so can save the user.
-                System.out.println("No current users with email " + newUser.getEmail());
 
                 //insert new user into db
                 PreparedStatement stmt2 = conn.prepareStatement("INSERT INTO users VALUES (NULL, ?, ?, ?, ?)");
@@ -36,6 +36,8 @@ public class JSONController {
                 stmt2.setString(3, newUser.getEmail());
                 stmt2.setString(4, newUser.getPassword());
                 stmt2.execute();
+
+                System.out.println("New User inserted into db with email " + newUser.getEmail());
 
                 //prepare the container to return with user and null error message
                 loginRegReturnContainer = new LoginRegReturnContainer(newUser, null);
@@ -50,6 +52,46 @@ public class JSONController {
         } catch (SQLException ex) {
             System.out.println("Exception caught in register endpoint");
             ex.printStackTrace();
+        }
+
+        return loginRegReturnContainer;
+    }
+
+    @RequestMapping(path = "/login.json", method = RequestMethod.POST)
+    public LoginRegReturnContainer login(@RequestBody User user) {
+        System.out.println("\nIn login method in JSON controller");
+        LoginRegReturnContainer loginRegReturnContainer = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:h2:./main");
+            //check if the user is in the database (by email)
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE email = ?");
+            stmt.setString(1, user.getEmail());
+            ResultSet resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                //email is attached to a user in our db
+                //check to see if password matches
+                if (user.getPassword().equals(resultSet.getString("password"))) {
+                    //passwords match. Retrieve all fields for user object
+                    User retrievedUser = new User(resultSet.getString("firstName"), resultSet.getString("lastName"), user.getEmail(), user.getPassword());
+                    //prepare return container with retrieved user and null error message
+                    System.out.println("User logged in with email " + user.getEmail());
+                    loginRegReturnContainer = new LoginRegReturnContainer(retrievedUser, null);
+                } else {
+                    //passwords don't match
+                    String message = "Incorrect password for user with email " + user.getEmail();
+                    System.out.println(message);
+                    //prepare a return container with null user and error message
+                    loginRegReturnContainer = new LoginRegReturnContainer(null, message);
+                }
+            } else {
+                //no users with that email in our db
+                String message = "No users with email " + user.getEmail() + " in our database";
+                System.out.println(message);
+                //prepare a return container with null user and error message
+                loginRegReturnContainer = new LoginRegReturnContainer(null, message);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Exception caught in login endpoint");
         }
 
         return loginRegReturnContainer;
--- src/main/resources/static/js/ng-controller.js
+++ src/main/resources/static/js/ng-controller.js
@@ -27,7 +27,29 @@ angular.module('DailyChecklistApp', [])
                     });
         };
 
-        $scope.test = "Angular working!";
+        $scope.login = function(loginEmail, loginPassword) {
+            console.log("In login function in ng controller");
+
+            var returningUserInfo = {
+                email: loginEmail,
+                password: loginPassword
+            }
+
+            $http.post("/login.json", returningUserInfo)
+                .then(
+                    function successCallback(response) {
+                        console.log(response.data);
+                        if (response.data.errorMessage == null) {
+                            $scope.currentUser = response.data.user;
+                            $scope.loginOrRegErrorMessage = "";
+                        } else {
+                            $scope.loginOrRegErrorMessage = response.data.errorMessage;
+                        }
+                    },
+                    function errorCallback(response) {
+                        console.log("Unable to get data...");
+                    });
+        };
 
 
    });
